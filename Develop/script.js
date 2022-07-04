8a4e1e474eafc562bf89cffc679125fca1c895c1
Boyd007 <105176171+Boyd007@users.noreply.github.com>
2022-07-04 16:34:17 -0400

    finalcode

--- Develop/script.js
+++ Develop/script.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
   $('.saveBtn').on('click', function () {
-    localStorage.setItem($(this).prev().attr("id"), $(this).prev().val());
+    localStorage.setItem($(this).parent().attr("id"), $(this).prev().val());
 });
 
   $("#hour8 .description").val(localStorage.getItem("hour8"));
